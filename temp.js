09051b74205074e89c4b6f8eb644c67e6ba655b9
KillerWBI <108660418+KillerWBI@users.noreply.github.com>
2022-08-17 12:55:11 +0200

    Update temp.js

--- temp.js
+++ temp.js
@@ -1,4 +1,4 @@
-let winterDays = [-48, -46, 48, 27, -20, -35, 43, 4, 9, 10, 41,
+ let winterDays = [-48, -46, 48, 27, -20, -35, 43, 4, 9, 10, 41,
      -46, -4, 0, -38, -49, 25, -46, -48, -23, -25, -22, 12, 38,
       19, -20, 26, 4, 19, 23, 26, -41, 4, -13, -9, -11, -7, 38,
        27, 41, 14, -35, -38, -44, -44, -22, -24, 29, -32, 41,
@@ -7,24 +7,28 @@ let winterDays = [-48, -46, 48, 27, -20, -35, 43, 4, 9, 10, 41,
           -48, 40, -37, -44, 0, 46, 36, -34, -50, 8, -3, 26, 
           40, 10, -36, 24];
 
-          let temp = 0;
-for(let i = 0 ; i <= winterDays.length ; i++){
-    
-        if(winterDays[i] > 0  && winterDays[i + 1] < 0){
-            temp++
+let prom = 0 ;
+let maxprom = 0;
+ for( let i = 0; i < winterDays.length; i++){
+     if(winterDays[i] < 0){
+         prom++;
+     } else {
+          if(prom > maxprom){
+             maxprom = prom;
+          }
+        prom = 0;  
      }
-}
+ }
 
-console.log(temp)
-alert(`всего заморозков: ${temp} дней.`)
+console.log(`Max period Cold: ${maxprom} days.`)
 
-//let UkrAvtoDorDays = 0;
+     //let temp = 0;
+//for(let i = 0 ; i <= winterDays.length ; i++){
+    
+        //if(winterDays[i] > 0  && winterDays[i + 1] < 0){
+           // temp++
+//     }
+//}
 
- //   for(let i = 0; i < winterDays.length - 1; i++){
-
- //       if(winterDays[i] >= 0 && winterDays[i+1] < 0 ){
- //           UkrAvtoDorDays++;
- //       }
- //   }
-
- //   console.log(`UkrAvtoDorDays: ${UkrAvtoDorDays} days.`);
+//console.log(temp)
+//alert(`всего заморозков: ${temp} дней.`)
